e69a90b0174d34febc939953cedcfdc5489d145e
Andrew J. Pounds <pounds_aj@mercer.edu>
2018-02-27 05:05:26 -0500

    added some comments to mmm.c and cleaned it up

--- pthreads/mmm.c
+++ pthreads/mmm.c
@@ -1,18 +1,33 @@
 
+/**************************************************************
+ *
+ * A matrix-matrix multiplication function designed
+ * to work optimally over a given number of threads using
+ * the POSIX Threads library (pthreads)
+ *
+ * Prof. Andrew J. Pounds, Ph.D.
+ * Mercer University
+ * Spring 2018
+ *
+ **************************************************************/
+
 #ifdef __cplusplus
 extern "C" {
 #endif
     void mmm_( int *threads, int *len,  double *a, double *b, double*c );
 #ifdef __cplusplus
-    }
+}
 #endif
-
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
 
+/* Function prototypes */
+
 void *mmm_thread_worker();
+
+/* Struct for passing data to thread functions */
 
 struct args {
     int N; 
@@ -39,32 +54,32 @@ void mmm_( int *threads, int *len, double *A, double *B, double *C ){
 
     // if there are fewer dimensions than threads, do a simple single-threaded matrix multiplication.
     if ( matrixDimension < numThreads ) {
-       for (int i=0;i<matrixDimension;i++) {
-           for (int j=0;j<matrixDimension;j++) {
-               *(C+(i*matrixDimension+j))=0.0;
-               for (int  k=0;k<matrixDimension;k++) {
-                   *(C+(i*matrixDimension+j)) += *(A+(i*matrixDimension+k)) * *(B+(k*matrixDimension+j));
-               }
-           }  
-       } 
+        for (int i=0;i<matrixDimension;i++) {
+            for (int j=0;j<matrixDimension;j++) {
+                *(C+(i*matrixDimension+j))=0.0;
+                for (int  k=0;k<matrixDimension;k++) {
+                    *(C+(i*matrixDimension+j)) += *(A+(i*matrixDimension+k)) * *(B+(k*matrixDimension+j));
+                }
+            }  
+        } 
     }
 
     else { 
 
-    /* 
-     * Now the parallel work begins.  The process is to first determine how to break
-     *  up the matrix work equitably across the threads.  Once this is done the struct is filled with
-     *  the information and a thread is started using the information.  Other than the size of the
-     *  matrices and the rows to be processed, only the pointers to the memory locations of the matrices
-     *  are passed.
-     */ 
+        /* 
+         *  The parallel work begins.  The process is to first determine how to break
+         *  up the matrix work equitably across the threads.  Once this is done the struct is filled with
+         *  the information and a thread is started using the information.  Other than the size of the
+         *  matrices and the rows to be processed, only the pointers to the memory locations of the matrices
+         *  are passed.
+         */ 
 
         // Malloc an array to keep up with thread id's for each thread
         thread_id = (pthread_t *) malloc (numThreads * sizeof(pthread_t));
 
         // Malloc an array to keep up with how many rows to work on in each thread
         numberOfRows = ( int * ) malloc( numThreads * sizeof(int) );
- 
+
         // Here we detemine the number of rows over which each thread will work
         for (int i=0; i<numThreads; i++ ){
             *(numberOfRows+i) = matrixDimension / numThreads;
@@ -73,7 +88,9 @@ void mmm_( int *threads, int *len, double *A, double *B, double *C ){
             *(numberOfRows+i) = *(numberOfRows+i) + 1;
         }
 
-
+        // Now that we know how many rows each thread will be responsible for computing,
+        // malloc memory for the struct data, pack the struct with the thread-specific info
+        // on where it is to start and stop processing, and create the thread using this data. 
         stopRow=0;
         for(int i=0; i < numThreads ; i++) {
             {   
@@ -91,43 +108,52 @@ void mmm_( int *threads, int *len, double *A, double *B, double *C ){
             }
         }
         for(int i=0; i < numThreads ; i++) {
-          pthread_join( *(thread_id+i), NULL); 
+            pthread_join( *(thread_id+i), NULL); 
         }
 
         free(numberOfRows);
         free(thread_id);
     }
 
-    }
+    // END OF MMM -- the memory pointed to by *C should now contain the product A.B
+}
 
 
-       void *mmm_thread_worker( struct args *thread_args  ) {
+void *mmm_thread_worker( struct args *thread_args  ) {
 
-       int i, j, k;
-       double val;
-       int rowStart, rowStop, N; 
-       double *A, *B, *C;
-        
-       N        =  thread_args->N;
-       rowStart =  thread_args->startRow;
-       rowStop  =  thread_args->stopRow; 
-       A        =  thread_args->Aptr;
-       B        =  thread_args->Bptr;
-       C        =  thread_args->Cptr;
+    // This is where the actual work of multiplying each matrix takes place --
+    // with each mmm_thread_worker only responsible for a piece of the matrix A.
+    
+    // It is worth noting here that while the thread workers are reading simultaneously
+    // from *A and *B, they never write to same memory locations in *C.
 
-       for (i=rowStart;i<rowStop;i++) {
-           for (j=0;j<N;j++) {
-               *(C+(i*N+j))=0.0;
-               for (k=0;k<N;k++) {
-                   *(C+(i*N+j)) += *(A+(i*N+k)) * *(B+(k*N+j));
-               }
-           }  
-       } 
+    int i, j, k;
+    double val;
+    int rowStart, rowStop, N; 
+    double *A, *B, *C;
 
-     free(thread_args);
-     pthread_exit(NULL);
-     }
-     
+    // Unpack the thread_args struct into normal variables
+    N        =  thread_args->N;
+    rowStart =  thread_args->startRow;
+    rowStop  =  thread_args->stopRow; 
+    A        =  thread_args->Aptr;
+    B        =  thread_args->Bptr;
+    C        =  thread_args->Cptr;
+
+    // Process the rows for which this thread is responsible
+    for (i=rowStart;i<rowStop;i++) {
+        for (j=0;j<N;j++) {
+            *(C+(i*N+j))=0.0;
+            for (k=0;k<N;k++) {
+                *(C+(i*N+j)) += *(A+(i*N+k)) * *(B+(k*N+j));
+            }
+        }  
+    } 
+
+    free(thread_args);
+    pthread_exit(NULL);
+}
+
 
 
 
